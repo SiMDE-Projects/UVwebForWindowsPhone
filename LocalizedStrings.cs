411b38e357eeae8753bc3dbf58e52188a882f248
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-14 17:21:33 +0200

    First commit (basic Pivot application)

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using UVwebForWindowsPhone.Resources;
+
+namespace UVwebForWindowsPhone
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
