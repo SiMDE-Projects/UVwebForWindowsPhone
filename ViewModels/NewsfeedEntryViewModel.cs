80c4e8dd469a7300d356f8baf8c041004f715732
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-15 19:00:58 +0200

    Complete NewsFeedEntryViewModel

--- ViewModels/NewsfeedEntryViewModel.cs
+++ ViewModels/NewsfeedEntryViewModel.cs
@@ -12,8 +12,77 @@ namespace UVwebForWindowsPhone.ViewModels
 {
     public class NewsfeedEntryViewModel : INotifyPropertyChanged
     {
-        
-      
+        /// <summary>
+        /// A simple newsfeed item view model to be displayed in the newsfeed.
+        /// </summary>
+        /// <returns></returns>
+        private string _author;
+        public string Author
+        {
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                if (value != _author)
+                {
+                    _author = value;
+                    NotifyPropertyChanged("Author");
+                }
+            }
+        }
+
+        private string _date;
+        public string Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                if (value != _date)
+                {
+                    _date = value;
+                    NotifyPropertyChanged("Date");
+                }
+            }
+        }
+
+        private string _comment;
+        public string Comment
+        {
+            get
+            {
+                return _comment;
+            }
+            set
+            {
+                if (value != _comment)
+                {
+                    _comment = value;
+                    NotifyPropertyChanged("Comment");
+                }
+            }
+        }
+
+        private string _action;
+        public string Action
+        {
+            get
+            {
+                return _action;
+            }
+            set
+            {
+                if (value != _action)
+                {
+                    _action = value;
+                    NotifyPropertyChanged("Comment");
+                }
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged(String propertyName)
