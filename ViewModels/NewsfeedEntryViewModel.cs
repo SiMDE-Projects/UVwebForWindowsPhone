2d0468c97e0cb23bdc0477b3dac4add9eeb69ad0
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-15 16:19:59 +0200

    Add NewsFeedEntryViewModel

---
+++ ViewModels/NewsfeedEntryViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace UVwebForWindowsPhone.ViewModels
+{
+    public class NewsfeedEntryViewModel : INotifyPropertyChanged
+    {
+        
+      
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
